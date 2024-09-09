6015dc3c1341f8a03228a30a27cd9b3670690229
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2024-09-09 14:25:01 +0300

    Refactor endpoints and Model

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,6 +8,6 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->post('tasks', 'TaskController::create');       // Create
 $routes->get('tasks', 'TaskController::index');         // Read all tasks
-$routes->get('tasks/(:id)', 'TaskController::show/$1'); // Read single task
-$routes->put('tasks/(:id)', 'TaskController::update/$1'); // Update
-$routes->delete('tasks/(:id)', 'TaskController::delete/$1'); // Delete
+$routes->get('tasks/(:num)', 'TaskController::show/$1');            // Read single task
+$routes->put('tasks/(:num)', 'TaskController::update/$1');          // Update
+$routes->delete('tasks/(:num)', 'TaskController::delete/$1'); 
